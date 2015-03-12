2d5f941f98025f8a38418f1880374675d465b37c
shybyte <ein@volloeko.de>
2015-03-12 07:35:24 +0100

    Check with Language Tool from server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var http = require('http');
 var express = require('express');
+var Promise = require('bluebird');
+var request = Promise.promisify(require('request'));
 
 function pathInsideProjectRoot(pathFromProjectRoot) {
   return path.join(__dirname, '..', pathFromProjectRoot);
@@ -16,10 +18,28 @@ app.use('/bower_components', express.static(pathInsideProjectRoot('bower_compone
 app.use('/compiled', express.static(pathInsideProjectRoot('.tmp/compiled')));
 app.use('/client', express.static(pathInsideProjectRoot('client'))); // source-maps
 
+
 app.use('/api', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify({test: 'Hossa'}));
 });
+
+
+app.use('/check', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  request({
+    url: 'http://localhost:8081',
+    method: 'POST',
+    form: {
+      language: 'EN-US',
+      text: 'errorr'
+    }
+  }).then(function (checkReportXML) {
+    console.log('checkReportXML', checkReportXML);
+  });
+  res.end(JSON.stringify({test: 'CheckResult'}));
+});
+
 
 var server = http.createServer(app);
 server.listen(port);
