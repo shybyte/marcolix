f42ab35169c73193d0155592c1ea8b2b98e62eb8
shybyte <ein@volloeko.de>
2015-03-18 07:49:25 +0100

    Expand card

--- client/scripts/sidebar.ts
+++ client/scripts/sidebar.ts
@@ -5,9 +5,12 @@ module marcolix {
 
   interface IssueComponentProps {
     issue: Issue
+    onClick: (index:number) => void
+    index: number
+    expanded: boolean
   }
 
-  function makeSuffixPrefixWhiteSpaceVisible(s: string) {
+  function makeSuffixPrefixWhiteSpaceVisible(s:string) {
     if (/^\s|\s$/.test(s)) {
       return '"' + s + '"';
     } else {
@@ -19,10 +22,28 @@ module marcolix {
     state = {
       isMouseOverReplacementInTitle: false,
       isMouseOverReplacementPopup: false,
-      replacementPopupPositionLeft: 0
+      replacementPopupPositionLeft: 0,
+      replacementsInBodyMarginLeft: 0
+    }
+
+
+    componentWillReceiveProps = (nextProps:IssueComponentProps) => {
+      var issueBody = <HTMLElement> React.findDOMNode(this.refs['issueBody']);
+      var positionOfReplacementInTitleLeft = React.findDOMNode(this.refs['replacementInTitle'])['offsetLeft'];
+      if (nextProps.expanded) {
+        var issueBodyContent = <HTMLElement> React.findDOMNode(this.refs['issueBodyContent']);
+        var bodyHeight = issueBodyContent.offsetHeight + 20;
+        issueBody.style.maxHeight = bodyHeight + 'px';
+        this.setState({replacementsInBodyMarginLeft: positionOfReplacementInTitleLeft});
+      } else {
+        issueBody.style.maxHeight = '0px';
+      }
     }
 
     onMouseOverReplacementInTitle = () => {
+      if (this.props.expanded) {
+          return;
+      }
       var positionOfReplacementInTitleLeft = React.findDOMNode(this.refs['replacementInTitle'])['offsetLeft'];
       this.setState({
         isMouseOverReplacementInTitle: true,
@@ -45,9 +66,21 @@ module marcolix {
     isReplacementPopupOpen = () => (this.state.isMouseOverReplacementInTitle || this.state.isMouseOverReplacementPopup)
 
     render() {
+      var p = this.props;
       var issue = this.props.issue;
-      return div({className: 'issue', title: this.props.issue.message},
-        div({className: 'issueTitle'},
+
+      function renderReplacementsTail() {
+        return issue.replacements.slice(1, 5).map((replacement, i) =>
+            span({className: 'replacement', key: i},
+              makeSuffixPrefixWhiteSpaceVisible(replacement)
+            )
+        )
+      }
+
+      return div({className: 'issue' + (p.expanded ? ' expanded' : ''), title: issue.message},
+        // title
+        div({className: 'issueTitle', onClick: () => p.onClick(p.index)},
+          span({className: 'icons'}, span({className: 'openCloseIcon'}, '')),
           span({className: 'surface'},
             makeSuffixPrefixWhiteSpaceVisible(issue.surface)),
           issue.replacements.length > 0 ?
@@ -61,6 +94,8 @@ module marcolix {
                 },
                 makeSuffixPrefixWhiteSpaceVisible(issue.replacements[0]))
             ) : null),
+
+        // replacementsPopupContainer
         issue.replacements.length > 1 ? div({className: 'replacementsPopupContainer'},
           div({
               className: 'replacementsPopup ' + ( this.isReplacementPopupOpen() ? 'open' : ''),
@@ -71,13 +106,22 @@ module marcolix {
                 left: this.state.replacementPopupPositionLeft
               }
             },
-            issue.replacements.slice(1, 5).map((replacement, i) =>
-                span({className: 'replacement', key: i},
-                  makeSuffixPrefixWhiteSpaceVisible(replacement)
-                )
-            )
+            renderReplacementsTail()
           )
-        ) : null
+        ) : null,
+
+        // body
+        div({className: 'issueBody', ref: 'issueBody'},
+          div({className: 'issueBodyContent', ref: 'issueBodyContent'},
+            div({
+                style: {
+                  marginLeft: this.state.replacementsInBodyMarginLeft
+                }
+              },
+              renderReplacementsTail()),
+            div({className: 'issueMessage'}, issue.message)
+          )
+        )
       );
     }
   }
@@ -89,15 +133,30 @@ module marcolix {
   }
 
   export class SidebarComponent extends React.Component<SidebarProps,any> {
+    state = {
+      expandedIssueIndex: -1
+    }
+    onClickIssue = (index:number) => {
+      console.log('Clicked on', index);
+      this.setState({expandedIssueIndex: index});
+    }
+
     render() {
       var p = this.props;
+      var state = this.state;
 
       if (!p.checkReport) {
         return div({}, 'No Check Result YET!')
 
       }
       return div({className: 'sidebar'},
-        p.checkReport.issues.map((issue, i) => SidebarFac({issue: issue, key: '' + i}))
+        p.checkReport.issues.map((issue, i) => SidebarFac({
+          onClick: this.onClickIssue,
+          issue: issue,
+          expanded: i === state.expandedIssueIndex,
+          index: i,
+          key: '' + i
+        }))
       );
     }
   }
