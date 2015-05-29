6d364eb1481453b567b3ac5b91e807d9c986b61b
shybyte <ein@volloeko.de>
2015-05-29 11:40:14 +0200

    Fix space before issue at sentence begin bug

--- server/report.ts
+++ server/report.ts
@@ -1,6 +1,7 @@
 'use strict';
 
 import _ = require('lodash');
+import utils = require('./utils');
 import Issue = marcolix.Issue;
 
 function startsWithWhiteSpace(s:string) {
@@ -10,11 +11,21 @@ function startsWithWhiteSpace(s:string) {
 function prependPrecedingWord(issue:Issue, error:languageTool.Error):Issue {
   var contextBefore = error.context.slice(0, parseInt(error.contextoffset));
   var wordBefore = contextBefore.replace(/^.*?(\S+\s*$)/, '$1');
-  var clonedIssue = _.clone(issue);
-  clonedIssue.surface = wordBefore + issue.surface;
-  clonedIssue.replacements = issue.replacements.map(r => wordBefore + r);
-  clonedIssue.range = [clonedIssue.range[0] - wordBefore.length, clonedIssue.range[1]]
-  return clonedIssue;
+  return utils.set(issue, iss => {
+    iss.surface = wordBefore + issue.surface;
+    iss.replacements = issue.replacements.map(r => wordBefore + r);
+    iss.range = [iss.range[0] - wordBefore.length, iss.range[1]]
+  });
+
+}
+
+function removeWhiteSpacesFromBegin(issue:Issue) {
+  var issueSurfaceWithoutWhiteSpaceAtBegin = _.trimLeft(issue.surface);
+  var numberOfWhiteSpacesAtBegin = issue.surface.length - issueSurfaceWithoutWhiteSpaceAtBegin.length;
+  return utils.set(issue, (iss:Issue) => {
+    iss.surface = issueSurfaceWithoutWhiteSpaceAtBegin;
+    iss.range = [iss.range[0] + numberOfWhiteSpacesAtBegin, iss.range[1]];
+  });
 }
 
 export function convertCheckReport(checkReportLanguageTool:languageTool.CheckReport):Issue[] {
@@ -31,6 +42,9 @@ export function convertCheckReport(checkReportLanguageTool:languageTool.CheckRep
       ruleId: error.ruleId
     };
     if (startsWithWhiteSpace(issue.surface)) {
+      if (offset === 0) {
+        return removeWhiteSpacesFromBegin(issue);
+      }
       return prependPrecedingWord(issue, error);
     } else {
       return issue;
